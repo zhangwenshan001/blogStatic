1e79fd84da16a92a5569569df2086adc923fc75a
wenshan.z1991@gmail.com <wenshan.z1991@gmail.com>
2024-02-06 23:47:18 +0900

    Add daily 49 simple solution by using java stream

--- 0001-0100/0049_GroupAnagrams.java
+++ 0001-0100/0049_GroupAnagrams.java
@@ -1,31 +1,10 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList<List<String>>() ;
-
-        Map<String, List<String>> map = new HashMap<String, List<String>>();
-        for (String s : strs) {
-            String key = stringSort(s);
-            if (map.containsKey(key)) {
-                map.get(key).add(s);
-            } else {
-                List<String> curList = new ArrayList<String>();
-                curList.add(s);
-                map.put(key, curList);
-            }
-        }
-
-        for (List<String> list : map.values()) {
-            result.add(list);
-        }
-
-        return result;
-    }
-    
-    public static String stringSort(String s) {
-        char[] chs = s.toCharArray();
-
-        Arrays.sort(chs);
-
-        return (new String(chs));
+        return new ArrayList<>(Arrays.asList(strs)
+        .stream().collect(Collectors.groupingBy(s -> {
+            char[] counts = new char[26];
+            for(char c : s.toCharArray()) counts[c - 'a']++;
+            return String.valueOf(counts);
+        })).values());
     }
 }
